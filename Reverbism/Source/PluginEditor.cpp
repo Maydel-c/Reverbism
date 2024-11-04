1365ef61362801a8243e389604459578112e9cc8
Maydel-c <165515798+Maydel-c@users.noreply.github.com>
2024-11-04 16:52:04 +0530

    placeholder knobs arranged on canvas

--- Reverbism/Source/PluginEditor.cpp
+++ Reverbism/Source/PluginEditor.cpp
@@ -15,7 +15,13 @@ ReverbismAudioProcessorEditor::ReverbismAudioProcessorEditor (ReverbismAudioProc
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    
+    addAndMakeVisible(&roomSizeSlider);
+    addAndMakeVisible(&widthSlider);
+    addAndMakeVisible(&dampnessSlider);
+    addAndMakeVisible(&drywetSlider);
+    
+    setSize (600, 250);
 }
 
 ReverbismAudioProcessorEditor::~ReverbismAudioProcessorEditor()
@@ -26,15 +32,22 @@ ReverbismAudioProcessorEditor::~ReverbismAudioProcessorEditor()
 void ReverbismAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll (juce::Colours::black);
 }
 
 void ReverbismAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    
+    using namespace juce;
+    auto bounds = getLocalBounds();
+    
+    bounds.removeFromLeft(bounds.getWidth() * 0.1);
+    bounds.removeFromRight(bounds.getWidth() * 0.111);
+
+    roomSizeSlider.setBounds(bounds.removeFromLeft(bounds.getWidth() * 0.25));
+    widthSlider.setBounds(bounds.removeFromLeft(bounds.getWidth() * 0.33));
+    dampnessSlider.setBounds(bounds.removeFromLeft(bounds.getWidth() * 0.5));
+    drywetSlider.setBounds(bounds);
 }
--- Reverbism/Source/PluginEditor.h
+++ Reverbism/Source/PluginEditor.h
@@ -11,6 +11,14 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct RotarySlider : juce::Slider
+{
+    RotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                  juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+    }
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +36,8 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     ReverbismAudioProcessor& audioProcessor;
+    
+    RotarySlider roomSizeSlider, widthSlider, dampnessSlider, drywetSlider;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ReverbismAudioProcessorEditor)
 };
